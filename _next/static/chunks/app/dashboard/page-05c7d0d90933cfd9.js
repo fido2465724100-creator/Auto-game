1c1ba5ab0ffc3a8c96bdd3188cdca61923e78665
Anton <drmykhailov@gmail.com>
2026-09-17 23:42:44 +0200

    deploy: add factory premises rent scaling and vehicle model aging

---
+++ _next/static/chunks/app/dashboard/page-05c7d0d90933cfd9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[105],{8524:(e,s,_)=>{Promise.resolve().then(_.bind(_,6287))}},e=>{var s=s=>e(e.s=s);e.O(0,[931,744,219,761,673,114,811,287,587,855,358],()=>s(8524)),_N_E=e.O()}]);
