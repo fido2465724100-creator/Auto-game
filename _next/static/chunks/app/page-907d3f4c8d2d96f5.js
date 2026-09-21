c49a307cc00a4827376224e2f4939e223538ed8b
Anton <drmykhailov@gmail.com>
2026-09-21 14:11:44 +0200

    deploy: historical material evolution and synthetic rubber update

--- _next/static/chunks/app/page-907d3f4c8d2d96f5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{91:(e,s,r)=>{Promise.resolve().then(r.bind(r,7246))},7246:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>t});var u=r(4568),n=r(7041);function t(){return(0,u.jsx)(n.default,{})}}},e=>{var s=s=>e(e.s=s);e.O(0,[692,898,744,262,701,673,811,114,41,587,855,358],()=>s(91)),_N_E=e.O()}]);
